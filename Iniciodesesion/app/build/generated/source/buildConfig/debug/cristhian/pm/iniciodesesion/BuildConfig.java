8b1422b3e360be94f39d02357764bfadd6e4c46e
cristhianmantuano <58351425+cristhianmantuano@users.noreply.github.com>
2019-11-30 17:25:58 -0500

    Practica de Inicio de sesion

---
+++ Iniciodesesion/app/build/generated/source/buildConfig/debug/cristhian/pm/iniciodesesion/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package cristhian.pm.iniciodesesion;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "cristhian.pm.iniciodesesion";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
